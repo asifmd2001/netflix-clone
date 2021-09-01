381625a98eeb07286d07b13fa81370c01c6fb4aa
asifmd2001 <asifmohamedsm2001@gmail.com>
2021-09-01 20:12:53 +0530

    spelling

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Nav />
       <Banner />
 
-      <Row title="NETFLIX ORIGINALS "
+      <Row title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
         isLarge />
       <Row title="Trending" fetchUrl={requests.fetchTrending} />
